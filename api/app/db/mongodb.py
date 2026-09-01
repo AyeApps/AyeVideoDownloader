61f1385103885a9fe9e6bff287107d62f7feb8d5
alberto24dev <alberto24dev@icloud.com>
2026-08-31 20:20:58 -0600

    fix(db): specify authSource=$external for MONGODB-X509 authentication

--- api/app/db/mongodb.py
+++ api/app/db/mongodb.py
@@ -31,6 +31,7 @@ async def init_db():
         client_kwargs["tls"] = True
         client_kwargs["tlsCertificateKeyFile"] = cert_path
         client_kwargs["authMechanism"] = "MONGODB-X509"
+        client_kwargs["authSource"] = "$external"
         logger.info(f"Connecting to MongoDB with X.509 Certificate ({cert_path})...")
 
     client = AsyncIOMotorClient(settings.mongodb_url, **client_kwargs)
